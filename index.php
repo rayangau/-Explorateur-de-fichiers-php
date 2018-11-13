81fa751eaf5c1877f29e44507dc7ee376f410bdb
lilyisadreamer <ophelie.donizetti@outlook.fr>
2018-11-13 08:44:41 +0100

    maj

---
+++ index.php
@@ -0,0 +1,9 @@
+<? php 
+
+$path = realpath('/xampp/htdocs');
+ 
+$objects = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($path), RecursiveIteratorIterator::SELF_FIRST);
+foreach($objects as $name => $object){
+   var_dump($name);
+
+?>
