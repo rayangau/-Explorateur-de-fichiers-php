af60baa141a267cc82823d4012261e47698aed20
Neareeves <alethesj@gmail.com>
2018-11-13 15:05:49 +0100

    yo

--- explorateur.php
+++ explorateur.php
@@ -1,79 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <title>Manipulation de fichiers</title>
+<meta charset="UTF-8">
+<title>Manipulation</title>
 </head>
 <body>
-<h1>Manip de fichiers PHP</h1>
-<!-- $myfile = fopen($wamp."tuto_balises/footer.php", "r"); -->
-
+<ul>
 <?php
 
-$wamp = "C:/wamp64/www/";
 
 
-$myfile = fopen($wamp."tuto_balises/footer.php", "r");
-
-if( !$myfile)
-exit ("ouverture du fichier impossible");
 
 
-while($str = fgets($myfile)){
-echo $str."<br>";
+
+if ($dossier = opendir('/var/www/html/-Explorateur-de-fichiers-php/')){
+while(false!==$fichier = readdir($dossier)){if($fichier !='.'&& $fichier != '..'){
+echo'<li><a href="http://http://localhost/-Explorateur-de-fichiers-php/explorateur.php/'.$fichier.'">'.$fichier.'</a></li>';
+}
+}
+closedir($dossier);
 }
 
-// $infos = file ("infos.txt");
-// print_r($infos);
-
-
-
-?>
-
-
-
-a
-
-if(!(fclose($myfile)))
-exit ("fermeture du fichier echoué");
-
-
-
-
-
-
-
-
 
 
 
 
 ?>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+</ul>
 </body>
-</html>
+</html> 
